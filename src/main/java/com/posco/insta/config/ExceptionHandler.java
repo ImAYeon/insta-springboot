1021b4fafdbf1d0f94e40444357a266001513a0a
ayeon <gudwodjssl@gmail.com>
2022-06-09 12:10:27 +0900

    docs exception file name modify

--- src/main/java/com/posco/insta/config/ExceptionHandler.java
+++
@@ -1,5 +0,0 @@
-package com.posco.insta.config;
-
-
-public class ExceptionHandler {
-}
---
+++ src/main/java/com/posco/insta/config/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.posco.insta.config;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+}
