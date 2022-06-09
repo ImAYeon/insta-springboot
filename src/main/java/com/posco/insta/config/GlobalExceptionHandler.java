743ffb9b70bf59f877b72a4e20746b298b739013
ayeon <gudwodjssl@gmail.com>
2022-06-09 12:18:22 +0900

    feat IillegalAccessException

--- src/main/java/com/posco/insta/config/GlobalExceptionHandler.java
+++ src/main/java/com/posco/insta/config/GlobalExceptionHandler.java
@@ -1,9 +1,21 @@
 package com.posco.insta.config;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler
+    @ExceptionHandler(value = IllegalAccessException.class)
+    public ResponseEntity<Map<String, Object>> illegalAccessException
+            (IllegalAccessException e){
+        Map<String, Object> map = new HashMap<>();
+        map.put("errMsg", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map); // BAD_REQUEST : 400
+    }
 }
